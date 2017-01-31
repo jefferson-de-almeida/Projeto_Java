67352deb28b6a1b1fd6bc152acd206f5cdadce9b
jefferson-de-almeida <jefferson.alguir@gmail.com>
2017-01-31 10:26:40 -0200

    Meu primeiro programa

---
+++ HelloWorld/OlaMundo.java
@@ -0,0 +1,11 @@
+
+public class OlaMundo{
+
+	public static void main(String [] args){
+		int x = 7, y;
+		long abc = 10000;
+		y = (int) abc;
+		System.out.println("Minha idade é: " + x +" "+ abc + " "+y);
+	}
+	
+}
